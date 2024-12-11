2f0e5d69b317075d44988d2ce973fd73825ca127
T0raT <tigerliu@bu.edu>
2024-12-10 19:52:12 -0500

    I forgot to commit, Added a noise background effect and an header for the portfolio.

---
+++ app/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="grid fixed top-0 w-full justify-center font-bold text-2xl tracking-[0.16em]">
+      <h2 className="p-3">TIGER&apos;S VIRTUAL PORTFOLIO WEBSITE</h2>
+    </div>
+  );
+}
