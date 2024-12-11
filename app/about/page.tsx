b67da5e5d1b6a5ea6a4d22c890725ca1f13344aa
T0raT <tigerliu@bu.edu>
2024-12-11 14:40:13 -0500

    Added clsx, cn.tsx, framer motion and example text on the about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,20 @@
 import Header from "../components/Header";
+import { RotateWords } from "../components/RotateWords";
 
 export default function About() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-vcr)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-vcr)] w-full">
       <Header />
-      <p>Test paragraph</p>
+      <div className="w-[500px] absolute top-50 left-50">
+        <p className="mb-2">I AM</p>
+        <RotateWords
+          text=""
+          words={["A FRONTEND DEVELOPER.", "A COMPUTER SCIENTIST.", "A ???"]}
+        />
+        <br />
+
+        <p>CURRENTLY, IM STUDYING THE THESE TOPICS </p>
+      </div>
     </div>
   );
 }
