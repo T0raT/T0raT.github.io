1da40d8d902761648d52239dd0521ae201717fd9
T0raT <tigerliu@bu.edu>
2025-02-06 13:07:24 -0500

    Added comments on project page for kanban implementation

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,10 +1,19 @@
 import NavBar from "../components/NavBar";
 
 function ProjectsDetails() {
+  /* The plan for this is to create something
+   * similar to ZZZ's interknot board, practically a kanban board.
+   *
+   * The card component should include
+   * 1. Image slideshow? Or just project image
+   *  - Constant width, dynamic height
+   * 2. Prject Description.
+   *  - Constant width, dynmaic height
+   * */
   return (
     <div>
       <NavBar />
-      Projects Section
+      <div className="kanban-card"></div>
     </div>
   );
 }
