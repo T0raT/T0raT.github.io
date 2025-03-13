5584125d1536a567d38d1c039c578ea20ee089f7
T0raT <tigerliu@bu.edu>
2025-03-13 16:09:13 -0400

    fix: my dumb dumb ass forgot to capitalize page names for nav bar hehehe

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -5,25 +5,28 @@ import { usePathname } from "next/navigation";
 const NavBar = () => {
   /**
    * Using url to check current page,
-   * so I can highlight the current page on nav bar
+   * so I can highlight the current page on nav bar.
+   *
+   * Did not need useEffect like I thought before.
+   * Component is called on each page so it reloads anyways.
    */
   const pathname = usePathname(); // Get the current path
 
   const items = [
-    { name: "About", href: "/about" },
-    { name: "Projects", href: "/projects" },
-    { name: "Contact", href: "/contact" },
+    { name: "ABOUT", href: "/about" },
+    { name: "PROJECTS", href: "/projects" },
+    { name: "CONTACT", href: "/contact" },
   ];
   return (
     <nav className="fixed w-[160px] left-0 top-0 h-full z-10 flex flex-col justify-center font-[family-name:var(--font-vcr)]">
       <ul>
         {items.map((item, index) => (
-          // Holy shit I think I way over complicated this
-          // I don't think I have to do this but im stubborn LOL
-          // But tbh this is a very ugly way of going about this nav bar
+          // This is a little more refined than before.
           <li key={index}>
             <Link href={item.href} key={item.href}>
-              {/* If list item is the current page, highlights background and renders the little triangle */}
+              {/* Just checks if current pathname matches the href in items.
+                  If it matches, highlight the current page.
+              */}
               {pathname === item.href ? (
                 <div className="link-container flex justify-between">
                   <div className="ml-3">►</div>
