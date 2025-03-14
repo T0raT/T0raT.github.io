a32b6c607a468eeb42c363e6d0832ae006a5675e
T0ra <tigerliu@bu.edu>
2025-03-13 22:40:39 -0400

    feat: componentnized (idk how to word this lol) the project card

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
 import NavBar from "../components/NavBar";
+import ProjectCard from "../components/ProjectCard.tsx"
 
 function ProjectsDetails() {
   /* The plan for this is to create something
@@ -14,30 +14,7 @@ function ProjectsDetails() {
   return (
     <>
       <NavBar />
-      <div className="top-80">
-        <div
-          className="kanban-card border-[3px] border-black hover:animate-borderPulse
-          rounded-tl-3xl rounded-tr-3xl rounded-bl-3xl w-80 flex flex-col overflow-hidden"
-        >
-          <div className="img-container relative">
-            <Image
-              src="/images/zenless_zone_zero.jpg"
-              alt="image here"
-              width={300}
-              height={0} // Height set dynamically with CSS
-              style={{ height: "auto" }} // Keeps aspect ratio
-              className="rounded-tl-[1.35rem] rounded-tr-[1.35rem] object-cover w-full"
-            />
-          </div>
-          <div className="description-container py-2 px-4 bg-[#222222] ">
-            <h2 className="project-title text-2xl">Example Project</h2>
-            <hr className="divider border-2 border-gray-50/20 rounded-3xl my-2 " />
-            <p className="project-description">
-              I used *insert 5 billion stacks* to build to *insert LLM wrapper*
-            </p>
-          </div>
-        </div>
-      </div>
+      <ProjectCard />
     </>
   );
 }
