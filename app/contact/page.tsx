9ddf2586b4a0b488993f99bf4c0c0e7869bfbb27
T0raT <tigerliu@bu.edu>
2025-04-02 15:09:48 -0400

    feat: added contacts section

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,19 @@
-import NavBar from "../components/NavBar";
-
 function ContactDetails() {
   return (
-    <div className="m-auto px-5">
-      Contact section under developement ᕙ(⇀‸↼‶)ᕗ
-      <NavBar />
+    <div className="m-auto px-5 text-center">
+      <p className="inline">tigerliu@bu.edu</p>
+      <br />
+      <br />
+
+      <a href="https://www.linkedin.com/in/daian-liu-a69990193/">
+        <u>LinkedIn</u>
+      </a>
+      <br />
+      <br />
+
+      <a href="https://github.com/T0raT">
+        <u>Github</u>
+      </a>
     </div>
   );
 }
