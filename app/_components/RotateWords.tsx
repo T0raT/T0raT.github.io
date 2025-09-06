84f579baaefbb79d90650bdae67410e19d6cc8bf
T0ra <tigerliu@bu.edu>
2025-09-05 23:51:28 -0400

    refactor: using private folders, added back old RotateWord component from before.

---
+++ app/_components/RotateWords.tsx
@@ -0,0 +1,39 @@
+"use client";
+import * as React from "react";
+import { AnimatePresence, motion } from "framer-motion";
+
+export function RotateWords({
+  text = "Rotate",
+  words = ["Word 1", "Word 2", "Word 3"],
+}: {
+  text: string;
+  words: string[];
+}) {
+  const [index, setIndex] = React.useState(0);
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % words.length);
+    }, 3000);
+    // Clean up interval on unmount
+    return () => clearInterval(interval);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return (
+    <div className="text-center tracking-tighter w-fit gap-1.5">
+      {text}{" "}
+      <AnimatePresence mode="wait">
+        <motion.p
+          key={words[index]}
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 20 }}
+          transition={{ duration: 0.3 }}
+          className="bg-white text-black px-2"
+        >
+          {words[index]}
+        </motion.p>
+      </AnimatePresence>
+    </div>
+  );
+}
