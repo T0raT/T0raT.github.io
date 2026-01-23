aa34b2dd6c203f976719c4db45e3e8da800f4061
T0raT <tigerliu@bu.edu>
2026-01-23 11:11:47 -0500

    refactor: moved sizes into object, removed unecessary imports, deleted dev branch

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import './style.css'
 import './css_reset.css'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl') as HTMLCanvasElement;
-const canvas_height = canvas?.clientHeight;
-const canvas_width = canvas?.clientWidth;
+const sizes = {
+  width: canvas?.clientWidth,
+  height: canvas?.clientHeight
+}
 
 // Scene
 const scene = new THREE.Scene();
@@ -21,33 +22,31 @@ const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh)
 
 // Camera
-if (canvas_height && canvas_width) {
-  const camera = new THREE.PerspectiveCamera(60, canvas_width / canvas_height, 0.1, 100);
-  camera.position.z = 2;
-  scene.add(camera)
+const camera = new THREE.PerspectiveCamera(60, sizes.width / sizes.height, 0.1, 100);
+camera.position.z = 2;
+scene.add(camera)
 
-  const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-  })
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas
+})
 
-  renderer.setSize(canvas_width, canvas_height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-  const clock = new THREE.Clock()
-  const tick = () => {
-    const elapsedTime = clock.getElapsedTime()
+const clock = new THREE.Clock()
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
 
-    mesh.position.z = Math.sin(elapsedTime);
-    mesh.rotation.y = elapsedTime * Math.PI;
+  mesh.position.z = Math.sin(elapsedTime);
+  mesh.rotation.y = elapsedTime * Math.PI;
 
-    // Render
-    renderer.render(scene, camera)
+  // Render
+  renderer.render(scene, camera)
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-  }
-  tick()
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
 }
+tick()
 
 function rngLetters() {
   const CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-=_+[],./';
